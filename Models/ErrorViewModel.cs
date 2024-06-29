9c765885e8b99387a708a0cad36e73e1c8000f64
Gia Bao <nguyenhiengiabao12@gmail.com>
2024-06-29 15:01:00 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcommerceMVC8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
